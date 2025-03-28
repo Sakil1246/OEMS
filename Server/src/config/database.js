b3bfff8bda7b27af041f5b8c0a332ebaa5076fe0
Sakil1246 <sakilahmed345677@gmail.com>
2025-03-28 21:08:07 +0530

    erased unneccessary things

--- Server/src/config/database.js
+++ Server/src/config/database.js
@@ -5,8 +5,9 @@ dotenv.config();
 const connectDB=async()=>{
     await mongoose.connect(process.env.MONGO_URL,{
         // to avoid deprecation warnings
-        useNewUrlParser:true,
-        useUnifiedTopology:true
+        //it is enabled by default  by node 4.0 version . so no need to write
+        // useNewUrlParser:true,
+        // useUnifiedTopology:true
 
     });
 };
