b37359d2a82fc5ad3e941888d484b2ca42aeef67
Sakil1246 <sakilahmed345677@gmail.com>
2025-04-20 04:21:05 +0530

    added edit exam api

--- Client/src/components/Teacher/Edit.exam.paper.jsx
+++ Client/src/components/Teacher/Edit.exam.paper.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { Basic_URL } from '../../utils/constants'
+import { useLocation } from 'react-router-dom'
 
 const Editexampaper = () => {
+  const location=useLocation();
+  const id=location.state;
+   
+  const fetchExam=async ()=>{
+    const getExam=await axios.get(Basic_URL+"teacher/exam/"+id,{withCredentials:true});
+    console.log(getExam.data.data);
+    
+  }
+  useEffect(()=>{
+    fetchExam();
+  },[]);
   return (
     <div>
       edit exam
